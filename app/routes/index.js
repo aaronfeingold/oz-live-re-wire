223feb03bb0458b1d92c6ab3b7378b05ee36429f
aaronfeingold <ajfeingold88@gmail.com>
2021-06-16 13:12:49 -0500

    removed jade files, front end will be react

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const getArtistEvents = require('../controllers/index.js')
-const ArtistEvent = require('../models/artist_event.model')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const artist_events = await getArtistEvents()
-  res.render('index', artist_events)
+  res.send(artist_events)
 });
 
 module.exports = router;
